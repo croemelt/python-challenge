e5721dd5af0dac770659c0df0d2ac407c69336e8
connor.roemelt <connor.roemelt@gmail.com>
2019-12-21 20:09:37 -0600

    submission

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -1,33 +1,79 @@
-# import modules for Operating System and CSV Support
 import os
 import csv
 
-# set file path
-csvpath = os.path.join('Resources','budget_data.csv')
+# link file path
+csvpath = os.path.join("Resources","budget_data.csv")
 
-print("Financial Analysis")
-print("--------------------------")
+# define variables 
+months = 0
+total_profit = 0
+profit_change = 0
+starting_amount = 0
 
-# opening csv
+# Create lists 
+profit = []
+monthly_value = []
+dates = []
+
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    
-    # skip header line
-    next(csvfile)
+    # skip header row
+    csv_header = next(csvreader)
 
-    # month counter
-    month_count = sum(1 for row in csvreader)
-    print(f'Total Months: {month_count}')
-    
-    # total value
-    total = 0
-    for row in csv.reader(csvfile):
-        total += int(row[1])
-    print(f'Total: $ {total}')
-    
-    # total value different
-    total_value = sum(int[2] for r in csv.reader(csvfile))
-    print(f'Total: $ {total_value}')
+    # for loop
+    for row in csvreader:    
+      # Month Counter
+      months = months + 1 
 
+      # Will need it when collecting the greatest increase and decrease in profits
+      dates.append(row[0])
 
+      # Calculating total profit
+      profit.append(row[1])
+      total_profit = total_profit + int(row[1])
+
+      # Calculate average chnage
+      final_profit = int(row[1])
+      monthly_change_profits = final_profit - starting_amount
+
+      # append month list
+      monthly_value.append(monthly_change_profits)
+
+      profit_change = profit_change + monthly_change_profits
+      starting_amount = final_profit
+
+      # Average profit calculation
+      average_change = (profit_change/months)
+      
+      # Tracing back to min/max corresponding values
+      greatest_increase = max(monthly_value)
+      greatest_decrease = min(monthly_value)
+
+      increase_date = dates[monthly_value.index(greatest_increase)]
+      decrease_date = dates[monthly_value.index(greatest_decrease)]
+
+# Print statement to match formatting shown in instructions  
+print(f'-----------------------------------------------------')
+print(f'Financial Analysis')
+print(f'-----------------------------------------------------')
+print(f'Total Months: {months}')
+print(f'Total Profits: $ {total_profit}')
+print(f'Average Change: ${int(average_change)}')
+print(f'Greatest Increase in Profits: {increase_date} $ {greatest_increase}')
+print(f'Greatest Increase in Profits: {decrease_date} $ {greatest_decrease}')
+print(f'-----------------------------------------------------')
+
+# Define state to be included in output text file
+print_out = (
+    f'-----------------------------------------------------\n'
+    f'Financial Analysis\n'
+    f'-----------------------------------------------------\n'
+    f'Total Months: {months}\n'
+    f'Total Profits: $ {total_profit}\n'
+    f'Average Change: ${int(average_change)}\n'
+    f'Greatest Increase in Profits: {increase_date} $ {greatest_increase}\n'
+    f'Greatest Increase in Profits: {decrease_date} $ {greatest_decrease}\n'
+    f'-----------------------------------------------------')
+with open('print_out.txt', 'w') as text:
+    text.write(print_out)
     
--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,21 +1,61 @@
-# import modules for Operating System and CSV Support
 import os
 import csv
 
-# set file path
-csvpath = os.path.join('Resources','election_data.csv')
+# Link for file path
+csvpath = os.path.join("Resources","election_data.csv")
 
-print("Election Results")
-print("--------------------------")
+# Define variables
+voters = 0
+# Create Lists
+candidates = []
+ind_candidate = []
+vote_count = []
+vote_percent = []
 
-# opening csv
+
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    # For Loop for voter count and adding candidates to candidates list
+    for row in csvreader:
+        # Count the total number of votes
+        voters = voters + 1
+        # Setting candidate list
+        candidates.append(row[2])
+    # Second loop for getting each candiate, votes, and percent
+    for i in set(candidates):
+        ind_candidate.append(i)
+        # Get to total votes per person
+        p = candidates.count(i)
+        vote_count.append(p)
+        # Voter percentage calculation
+        g = (p/voters)*100
+        vote_percent.append(g)
+
+    # Get winning vote count and winner    
+    winning_vote_count = max(vote_count)
+    winner = ind_candidate[vote_count.index(winning_vote_count)]
     
-    # skip header line
-    next(csvfile)
+# Print statement to match formatting shown in instructions 
+print(f'-------------------------')
+print(f'Election Results')   
+print(f'-------------------------')
+print(f'Total Votes: {voters}')    
+print(f'-------------------------')
+for k in range(len(ind_candidate)):
+            print(f'{ind_candidate[k]} :  {vote_percent[k]} % ( {vote_count[k]} )')
+print(f'-------------------------')
+print(f'The winner is: {winner}')
+print(f'-------------------------')
 
-    voter_count = sum(1 for row in csvreader)
-    print(f'Total Votes: {voter_count}')
 
-    print("--------------------------")
+with open('print_out.txt', 'w') as text:
+    f'Election Results\n'
+    f'---------------------------------------\n'
+    f'Total Votes: {voters}\n'
+    f'---------------------------------------\n"'
+    for k in range(len(set(ind_candidate))):
+        f'{ind_candidate[k]} :  {vote_percent[k]} % ( {vote_count[k]} )\n'
+    f'---------------------------------------\n'
+    f'The winner is: {winner}\n'
+    f'-------------------------'
